7f691b3d82f21e6a5f820e512591344feac51325
Pavel Sima <psimavel@gmail.com>
2022-05-11 16:16:55 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PKU Daily Limitter',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
